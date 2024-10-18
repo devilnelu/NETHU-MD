f2a2346eb509c9fb93f69388e313c93c23921267
devilnelu <chutisose@gmail.com>
2024-10-18 12:17:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "IvcgxZAa#C7wAUz6FPdqhk5bCUZkDYm8ZfNw6EuLcjkDnpk3UWe4",
+SESSION_ID: process.env.SESSION_ID || "0mNC1IAT#yczHeCGX8bpKQ4ZwtWrZyGjs-65GZ-n8ASmGx76aCJU",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/7f0d7a04a30a602307e3d.jpg",
 SUDO_NB: process.env.SUDO_NB || "94701363708",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
